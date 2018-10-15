a6da129d39da43be9c314d279be3ba96d2abbb94
Hiago Forte <hiagoforte1@hotmail.com>
2018-10-15 15:09:28 -0300

    inclusão While

---
+++ src/course/WhileEnquantoExemple.java
@@ -0,0 +1,19 @@
+// author hiagoforte.
+// objetivo: ler três números digitados pelo usuário e soma-los usando a repetição "for".
+
+package course;
+
+import java.util.Scanner;
+
+public class WhileEnquantoExemple {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+	
+	}
+
+}
